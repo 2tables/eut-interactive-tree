85c5b19057f586aef64018719290346a32c10d38
ZNomad <137446166+2tables@users.noreply.github.com>
2026-07-04 00:57:40 +0000

    added some more bs

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 async function getInfo(){
-    res = await fetch("./baseplates.json");
+    res = await fetch("./data/baseplates.json");
     data = await res.json();
     baseplates = data;
-    res = await fetch("./nodes.json");
+    res = await fetch("./data/nodes.json");
     data = await res.json();
     nodes = data;
     return {baseplates: baseplates, nodes: nodes};
 }
 async function bridge(){
     info = await getInfo();
+    render();
 }
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
 function render(){
-    
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    requestAnimationFrame(render);
 }
+bridge();
